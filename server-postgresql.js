efef740c294e99203b49cab090ae69e09ba2dbe7
kim-jongsoung <kim-jongsoung@users.noreply.github.com>
2025-08-26 13:07:20 +0900

    Fix: Add static file serving for /pa directory to serve partner application images

--- server-postgresql.js
+++ server-postgresql.js
@@ -32,6 +32,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/pa', express.static('pa'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
